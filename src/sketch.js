58cbde9ef96d59d1235414562f3f45d663118478
dhmmasson <5263585+dhmmasson@users.noreply.github.com>
2024-10-05 09:16:41 +0200

    feat(ui): add a start game menu
    close #5

--- src/sketch.js
+++ src/sketch.js
@@ -9,6 +9,14 @@ const Levels = {
   },
 };
 
+/** MENUS */
+const mainMenuStruct = {
+  buttons: [{ label: "Start Game", action: startGame, handle: null }],
+  title: "Main Menu",
+  buttonSize: 200,
+  buttonSpacing: 10,
+};
+
 const GameStates = {
   menu: "menu",
   playing: "playing",
@@ -24,8 +32,14 @@ const Game = {
       height: 100,
     },
   },
+  menus: {
+    main: mainMenuStruct,
+  },
+  currentMenu: "",
+  nextMenu: "main",
 };
 
+/** UI */
 //Resize canvas to fill the div
 function windowResized() {
   const size = select("#canvas").size();
@@ -34,6 +48,61 @@ function windowResized() {
   Game.board.size.width = minSize;
   Game.board.size.height = minSize;
   resizeCanvas(minSize, minSize);
+  // Force Menu refresh
+  Game.currentMenu = "";
+}
+
+function touchMoved() {
+  // do some stuff
+  return false;
+}
+
+function initMenu(menu) {
+  // Display the main menu
+  gui = createGui();
+  // gui.setTitle(menu.title);
+  let y = 50;
+  menu.buttons.forEach((button) => {
+    button.handle = createButton(
+      button.label,
+      Game.board.size.width / 2 - menu.buttonSize / 2,
+      y,
+      menu.buttonSize
+    );
+    y += menu.buttonSize + menu.buttonSpacing;
+  });
+  return gui;
+}
+
+function checkButtons() {
+  Game.menus[Game.currentMenu].buttons.forEach((button) => {
+    if (button.handle.isPressed) {
+      button.action();
+    }
+  });
+}
+
+function displayMenu() {
+  //Only display the menu if the game is in the menu state
+  if (Game.state !== GameStates.menu) {
+    return false;
+  }
+  // Create the menu on menu change
+  if (Game.currentMenu !== Game.nextMenu) {
+    initMenu(Game.menus[Game.nextMenu]);
+    Game.currentMenu = Game.nextMenu;
+  }
+
+  background(220);
+  drawGui();
+
+  // Check if buttons are pressed
+  checkButtons();
+}
+
+/** */
+function startGame() {
+  Game.state = GameStates.playing;
 }
 
 function setup() {
@@ -43,5 +112,6 @@ function setup() {
 }
 
 function draw() {
-  background(220);
+  background(10);
+  displayMenu();
 }
