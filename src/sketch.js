c331b1f2f6fe2c6ff262b47535a4e6c8a3ccd98f
dhmmasson <5263585+dhmmasson@users.noreply.github.com>
2024-10-05 07:33:28 +0200

    feat: start game level structure

--- src/sketch.js
+++ src/sketch.js
@@ -1,3 +1,24 @@
+/** Levels */
+const Levels = {
+  tutorial: {
+    level: 0,
+    title: "Tutorial",
+    board: {
+      sites: [],
+    },
+  },
+};
+
+const GameStates = {
+  menu: "menu",
+  playing: "playing",
+};
+
+const Game = {
+  level: Levels.tutorial,
+  state: "menu",
+  currentLevel: 0,
+};
 
 function setup() {
   createCanvas(400, 400);
