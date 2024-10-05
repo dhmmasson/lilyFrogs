fda40a26d90dd3e65f02aaf6eb8007c07ad49672
dhmmasson <5263585+dhmmasson@users.noreply.github.com>
2024-10-05 07:53:49 +0200

    feat(ui): Add canvas and resize functionality
    - Add a canvas element to the index.qmd file
    - Update the script sources for p5.js and p5.sound.js in index.qmd
    - Add a resize function to the sketch.js file to dynamically resize the canvas
    - Update the CSS to style the canvas element and the div container
    
    close #26

--- src/sketch.js
+++ src/sketch.js
@@ -18,10 +18,28 @@ const Game = {
   level: Levels.tutorial,
   state: "menu",
   currentLevel: 0,
+  board: {
+    size: {
+      width: 100,
+      height: 100,
+    },
+  },
 };
 
+//Resize canvas to fill the div
+function windowResized() {
+  const size = select("#canvas").size();
+  // Square Board
+  let minSize = min(size.width, size.height);
+  Game.board.size.width = minSize;
+  Game.board.size.height = minSize;
+  resizeCanvas(minSize, minSize);
+}
+
 function setup() {
-  createCanvas(400, 400);
+  // Create canvas and put it in the canvas div to guess the size
+  createCanvas(Game.board.size.width, Game.board.size.height).parent("#canvas");
+  windowResized();
 }
 
 function draw() {
